1053563a21fe6c383e82e5328fa0ee2122fe98fa
Colin Brontén <colbro@elevad.umea.se>
2023-10-20 11:09:16 +0200

    done

---
+++ docs/_app/immutable/chunks/5-a79e0796.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-28ab8bd4.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,43 @@
 
 <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
 
-<h3 >Förnamn Efternamn</h3>
+<h3 class="name">Colin Bronten</h3>    
+
+<p id="inspo">
+  "The only way to learn a new programming language is by writing
+  programs in it." - Dennis Ritchie, creator of the C programming language.
+  </p>
+  <p style="background-color:pink">
+  "Every great developer you know got there by solving problems they
+  were unqualified to solve until they actually did it." - Patrick McKenzie,
+  software engineer and entrepreneur.
+  </p>
+
+
+
+  <style>
+
+#inspo{
+font-family: courier;
+font-size: 160%;
+}
+
+h1, h2, h3, p {
+  display: inline;
+  }
+
+
+h1{color:blue;}
+
+h2{color:purple;}
+
+h3{color:hotpink;}
+
+.name{
+  color:purple;
+  font-family:'Times New Roman', Times, serif;
+}
+
+</style>
+
 
